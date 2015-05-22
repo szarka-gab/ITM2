794adcbb5185ebf9e479bb00d4c98a6d376576c4
Gabriel SZARKA <szarka.gabriel@gmail.com>
2015-05-22 11:21:57 +0200

    VideoThumbnailGenerator works

--- gititm/assignment2/java/itm/video/VideoThumbnailGenerator.java
+++ gititm/assignment2/java/itm/video/VideoThumbnailGenerator.java
@@ -7,9 +7,24 @@ package itm.video;
 
 import itm.util.ImageCompare;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+import com.xuggle.mediatool.IMediaWriter;
+import com.xuggle.mediatool.ToolFactory;
+import com.xuggle.xuggler.ICodec;
+import com.xuggle.xuggler.IContainer;
+import com.xuggle.xuggler.IPacket;
+import com.xuggle.xuggler.IPixelFormat;
+import com.xuggle.xuggler.IRational;
+import com.xuggle.xuggler.IStream;
+import com.xuggle.xuggler.IStreamCoder;
+import com.xuggle.xuggler.IVideoPicture;
+import com.xuggle.xuggler.IVideoResampler;
+import com.xuggle.xuggler.Utils;
 
 /**
  * This class reads video files, extracts metadata for both the audio and the
@@ -25,7 +40,22 @@ import java.util.ArrayList;
  * thrown.
  */
 public class VideoThumbnailGenerator {
-
+	
+	ArrayList<IVideoPicture> allPictures = new ArrayList<IVideoPicture>();
+	ArrayList<BufferedImage> allPictures_Buff = new ArrayList<BufferedImage>();
+	
+	int framesTotal;
+	IContainer outContainer;
+	IStream outStream;
+	IStreamCoder outStreamCoder;
+	IStreamCoder videoCoder = null;
+	IRational frameRate;
+	  
+	int vWidth;
+	int vHeight;
+	
+	long firstTimeStamp=-1;
+  
 	/**
 	 * Constructor.
 	 */
@@ -84,7 +114,163 @@ public class VideoThumbnailGenerator {
 		}
 		return ret;
 	}
+	
 
+	@SuppressWarnings("deprecation")
+	protected void getImagesFromVideo(File input, int timeSpan){
+		
+		IContainer container = IContainer.make();
+
+		if (container.open(input.getAbsolutePath(), IContainer.Type.READ, null) < 0)
+		      throw new IllegalArgumentException("Cant open data: " + input.getAbsolutePath());
+		
+		int numStreams = container.getNumStreams();
+		
+		int videoStreamId = -1;
+
+	    for(int i = 0; i < numStreams; i++){
+	    	
+		      IStream stream = container.getStream(i);
+	
+		      IStreamCoder coder = stream.getStreamCoder();
+	
+		      if (coder.getCodecType() == ICodec.Type.CODEC_TYPE_VIDEO){
+		        videoStreamId = i;
+		        videoCoder = coder;
+		        break;
+		      }
+	    }
+
+	    if (videoStreamId == -1)
+	      throw new RuntimeException("VideoStream not found"+ input.getAbsolutePath());
+
+	    if (videoCoder.open() < 0)
+	      throw new RuntimeException("error opening videodecoder" + input.getAbsolutePath());
+
+	    IVideoResampler resampler = null;
+	    if (videoCoder.getPixelType() != IPixelFormat.Type.BGR24){
+
+	      resampler = IVideoResampler.make(videoCoder.getWidth(), videoCoder.getHeight(), IPixelFormat.Type.BGR24,videoCoder.getWidth(), videoCoder.getHeight(), videoCoder.getPixelType());
+	      
+	      if (resampler == null)
+	        throw new RuntimeException("Error creating color space" + input.getAbsolutePath());
+	    }
+
+	    IPacket packet = IPacket.make();
+	    
+	    while(container.readNextPacket(packet) >= 0){
+	      
+	      if (packet.getStreamIndex() == videoStreamId){
+	        IVideoPicture picture = IVideoPicture.make(videoCoder.getPixelType(),videoCoder.getWidth(), videoCoder.getHeight());
+
+	        int offset = 0;
+	        
+		        while(offset < packet.getSize()){
+		        	
+			          int bytesDecoded = videoCoder.decodeVideo(picture, packet, offset);
+			          
+			          if (bytesDecoded < 0)
+			            throw new RuntimeException("error decoding video: " + input.getAbsolutePath());
+			          
+			          offset += bytesDecoded;
+			          
+			          if (picture.isComplete()){
+			            IVideoPicture newPic = picture;
+		
+			            if (resampler != null){
+			            	
+			              newPic = IVideoPicture.make(resampler.getOutputPixelFormat(), picture.getWidth(), picture.getHeight());
+			              
+			              if (resampler.resample(newPic, picture) < 0)
+			                throw new RuntimeException("error resampling video: " + input.getAbsolutePath());
+			            }
+		
+			            if (newPic.getPixelType() != IPixelFormat.Type.BGR24)
+			              throw new RuntimeException("error decoding video as BGR24 " + input.getAbsolutePath());
+		
+			            	allPictures.add(newPic);
+		
+			          }
+		        }
+	      }
+	      
+	      else{
+	        do{} 
+	        
+	        while(false);
+	      }
+	    }
+
+	    if (videoCoder != null){
+	    	
+		      videoCoder.close();
+		      videoCoder = null;
+	    }
+	    
+	    if (container !=null){
+	    	
+	      container.close();
+	      container = null;
+	      
+	    }
+	    
+	    
+	    //Timespan//
+    	
+    	if(timeSpan > 0){
+    		System.out.println("timespan =" + timeSpan);
+    		
+	    	 int numPictures = allPictures.size();
+	    	 int startWert = 0;
+	    	 
+	    	 for(int i = 0; i < (numPictures-1); i++){
+	    		 if (startWert == allPictures.get(i).getPts()/1000000){
+	
+	    			 BufferedImage pictureToAdd = Utils.videoPictureToImage(allPictures.get(i));
+	    			 allPictures_Buff.add(pictureToAdd);
+	    			
+	    			 startWert = startWert + timeSpan;
+	    		 }
+	    	 }
+    		
+    	}
+    	
+    	if(timeSpan == 0){
+    		
+	    		ArrayList<BufferedImage> tempList = new ArrayList<BufferedImage>();
+	    		
+		    		for(int i = 0; i < allPictures.size(); i++){
+		    			tempList.add(Utils.videoPictureToImage(allPictures.get(i)));
+		    		}
+		    		
+	    		for(int i = 0; i < allPictures.size(); i = i + 20){
+	
+	    		BufferedImage toCompare = Utils.videoPictureToImage(allPictures.get(i));
+	    		allPictures_Buff.add(toCompare);
+	    		
+		    		for(int j = i; j < allPictures.size(); j= j + 3){
+		    			
+		    			BufferedImage compPic = tempList.get(j);
+		    			
+		    			ImageCompare tool = new ImageCompare(toCompare,compPic);
+		    			tool.compare();
+		    			
+		    			if(tool.match() == false ){
+		    				System.out.println("treffer");
+		    				
+		    				allPictures_Buff.add(compPic);
+		    				i = j;
+		    				break;
+		    			}
+	    			
+		    		}
+	    		}
+	    	}
+		    
+    	framesTotal = allPictures_Buff.size();
+    	
+		}
+	
 	/**
 	 * Processes the passed input video file and stores a thumbnail of it to the
 	 * output directory.
@@ -107,26 +293,42 @@ public class VideoThumbnailGenerator {
 		if (!output.isDirectory())
 			throw new IOException(output + " is not a directory!");
 
-		// create output file and check whether it already exists.
 		File outputFile = new File(output, input.getName() + "_thumb.swf");
 
 		// ***************************************************************
 		// Fill in your code here!
 		// ***************************************************************
 
-		// extract frames from input video
-
-		// create a video writer
-
-		// add a stream with the proper width, height and frame rate
 		
-		// if timespan is set to zero, compare the frames to use and add 
-		// only frames with significant changes to the final video
+		getImagesFromVideo(input,timespan);
 
-		// loop: get the frame image, encode the image to the video stream
+		String outFile = outputFile.getAbsolutePath();
+
+		vHeight = allPictures_Buff.get(0).getHeight();
+		vWidth = allPictures_Buff.get(0).getWidth();
+		        		
+		IMediaWriter outWriter = ToolFactory.makeWriter(outFile);
+		outWriter.addVideoStream(0,0, ICodec.ID.CODEC_ID_FLV1,vWidth,vHeight);
 		
-		// Close the writer
+		long startTime = System.nanoTime();
+		int lange = framesTotal;
+		int j = 0;
+		for (int i = 0; i < 64*lange ; i++){
+			
+			outWriter.encodeVideo(0, allPictures_Buff.get(j), System.nanoTime()-startTime, TimeUnit.NANOSECONDS);
 
+				if(i%64 == 0){
+					j++;
+					if(j >= framesTotal){
+						break;
+					}
+				}
+			
+		}
+		        
+		outWriter.flush();
+		outWriter.close();
+		
 		return outputFile;
 	}
 
